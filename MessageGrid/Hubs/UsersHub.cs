827e328f498ce2f9c419ca7ed3ffdab0eba45510
CSharpYouDull <SteveRuben@Gmail.com>
2013-05-04 15:55:25 -0500

    added authentication to persistent connection from SignalR

--- MessageGrid/Hubs/UsersHub.cs
+++ MessageGrid/Hubs/UsersHub.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Security;
+using MessageGrid.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace MessageGrid.Hubs
 {
     public class UsersHub:PersistentConnection
     {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            return !IsAuthenticated(request) ? null : base.OnConnected(request, connectionId);
+        }
+
         protected override Task OnReceived(IRequest request, string connectionId, string data)
         {
             return Connection.Broadcast(data);
+        }
+
+        private bool IsAuthenticated(IRequest request)
+        {
+            if (!request.Cookies.ContainsKey(FormsAuthentication.FormsCookieName))
+                return false;
+
+            var encryptedTicket = request.Cookies[FormsAuthentication.FormsCookieName];
+            var ticket = FormsAuthentication.Decrypt(encryptedTicket.Value);
+            var authenticated = ticket != null && !string.IsNullOrWhiteSpace(ticket.Name);
+            
+            return authenticated;
         }
     }
 }
--- MessageGrid/Scripts/app/main.js
+++ MessageGrid/Scripts/app/main.js
@@ -1,10 +1,4 @@
 ﻿$(document).ready(function () {
-    //for the purposes of this demo i'm executing these calls at application start
-    //for a real world application you would obviously want to wait until after authentication was successful
-    //var repo = new UserRepository();
-    //repo.GetUsers();
-    //repo.ListenForChanges();
-
     //start application navigation
     var nav = Navigation();
     nav.Routing.run('#/login');
